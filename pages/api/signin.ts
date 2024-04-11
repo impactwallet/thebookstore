44491982091f06a4811c61ea2c2a564a83fc67ca
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2024-04-11 19:52:56 +0200

    remove deplan

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { DePlanClient } from "deplan-client";
 
 // Sign In API endpoint
 export default function handler(
@@ -11,9 +10,6 @@ export default function handler(
       error: "Method not allowed",
     });
   }
-
-  const deplanClient = new DePlanClient('9KSfkquHqtDGq4uzEjwg1AhYynzc9zFCAAQCVZb2hkTn');
-  deplanClient.verifySignIn(req.body.message, req.body.signature, '7qUPhUmL6nNTWU7yMsWueR778SYbNhBU2B2tqddfns6j');
 
   res.status(200).json({ token: 'token' });
 }
